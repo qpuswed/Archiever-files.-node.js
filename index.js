5a851d92509460a70c5ac4d8a7a0e072a7d88bfc
qpuswed <qpuswedwork@egmail.com>
2025-06-24 20:23:52 +0100

    Changed [index] to file

--- index.js
+++ index.js
@@ -6,12 +6,9 @@ const path = "./dist/numb1.txt"
 let files = ["./dist/numb1.txt","./dist/numb2.txt","./dist/numb3.txt","./dist/numb4.txt","./dist/numb5.txt","./dist/numb6.txt","./dist/numb7.txt","./dist/numb8.txt","./dist/numb9.txt","./dist/numb10.txt"
 ]
 
-let index = 0;
-
 for (let file of files){
-    index++;
     let readableStream = fs.createReadStream(file);
-    let writeableWrite = fs.createWriteStream(`./dist/numb${index}.txt.gz`);
+    let writeableWrite = fs.createWriteStream(`${file}.gz`);
     readableStream.pipe(createGzip()).pipe(writeableWrite).on("finish", () => {
      console.log(`Файл успешно сжат и сохранён как ${file}.gz`)});
 }
